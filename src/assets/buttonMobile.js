d3b6c85674afbe86e9211e69339e37d09b75df78
Boulay.M <macboulay2@gmail.com>
2025-10-16 08:15:56 +0200

    mobile prevent

--- src/assets/buttonMobile.js
+++ src/assets/buttonMobile.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
   if (isMobile) {
-    const links = document.querySelectorAll("a.prevent-mobile");
+    const links = document.getElementsByClassName("prevent-mobile");
 
     links.forEach(link => {
       link.addEventListener("click", function (event) {
--- src/components/CardsPortfolio.jsx
+++ src/components/CardsPortfolio.jsx
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
   if (isMobile) {
-    const links = document.querySelectorAll("a.prevent-mobile");
+    const links = document.getElementsByClassName("prevent-mobile");
 
     links.forEach(link => {
       link.addEventListener("click", function (event) {
